edd055571dbf44345b7c9b691912bae59791dbc4
加藤嵩大 <takahiro@katoushiori-no-MacBook-Air.local>
2024-03-22 23:50:56 +0900

    WIP FIX COMMIT

--- issen_miner.py
+++ issen_miner.py
@@ -5,7 +5,6 @@
 # file license http://www.opensource.org/licenses/mit-license.php.
 
 
-from signal import signal, SIGINT
 import context as ctx 
 import traceback 
 import threading
@@ -18,7 +17,6 @@ import socket
 import time
 import json
 import sys
-import os
 
 import numpy as np
 
@@ -46,10 +44,10 @@ def logg(msg):
 
 
 
-def get_current_block_height():
+#def get_current_block_height():
     # returns the current network height 
-    r = requests.get('https://blockchain.info/latestblock')
-    return int(r.json()['height'])
+    #r = requests.get('https://blockchain.info/latestblock')
+    #return int(r.json()['height'])
 
 
 def calculate_hashrate(nonce, last_updated):
@@ -94,8 +92,7 @@ class ExitedThread(threading.Thread):
             try:
                 self.thread_handler2(arg)
             except Exception as e:
-                logg("ThreadHandler()")
-                logg(e)
+                pass
             ctx.listfThreadRunning[n] = False
 
             pass
@@ -122,28 +119,31 @@ def bitcoin_miner(restarted=True):
     global working_now
 
     target = (ctx.nbits[2:]+'00'*(int(ctx.nbits[:2],16) - 3)).zfill(64)
-    ctx.extranonce2 = hex(random.randint(0,2**32-1))[2:].zfill(2*ctx.extranonce2_size)      # create random
+    print(target)
+    ctx.extranonce2 = hex(random.randint(0,2**32-1))[2:].zfill(2*int(ctx.extranonce2_size, 16))
 
     coinbase = ctx.coinb1 + ctx.extranonce1 + ctx.extranonce2 + ctx.coinb2
     coinbase_hash_bin = hashlib.sha256(hashlib.sha256(binascii.unhexlify(coinbase)).digest()).digest()
 
     merkle_root = coinbase_hash_bin
-    for h in ctx.merkle_branch:
-        merkle_root = hashlib.sha256(hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()).digest()
-
+    #for h in ctx.merkle_branch:
+    #    merkle_root = hashlib.sha256(hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()).digest()
     merkle_root = binascii.hexlify(merkle_root).decode()
 
+    last_updated = int(time.time())
     #little endian
-    merkle_root = ''.join([merkle_root[i]+merkle_root[i+1] for i in range(0,len(merkle_root),2)][::-1])
+    print(merkle_root)
+    merkle_root = merkle_root[::-1]#''.join([merkle_root[i]+merkle_root[i+1] for i in range(0,len(merkle_root),2)][::-1])
+    print(merkle_root)
 
-    work_on = get_current_block_height()
-
-
-    ctx.nHeightDiff[work_on+1] = 0 
-
+    #work_on = get_current_block_height()
 
 
-    logg('[*] Working to solve block with height {}'.format(work_on+1))
+    #ctx.nHeightDiff[work_on+1] = 0 
+
+
+
+    #logg('[*] Working to solve block with height {}'.format(work_on+1))
 
     nonce_bit = 2 ** int(sys.argv[2]) # nonce array range
     last_nonce = nonce_bit
@@ -153,17 +153,21 @@ def bitcoin_miner(restarted=True):
     
     def calc_hash(nonce):
         nonce = hex(nonce)[2:].zfill(8)
-        blockheader = ctx.version + ctx.prevhash + merkle_root + ctx.ntime + ctx.nbits + nonce +\
-        '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000'
-        hash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(blockheader)).digest()).digest()
-        hash = binascii.hexlify(hash).decode()
+        try:
+            blockheader = ctx.version + ctx.prevhash + merkle_root + ctx.ntime + ctx.nbits + nonce + '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000'
+            hash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(blockheader)).digest()).digest()
+            hash = binascii.hexlify(hash).decode()
+        except:
+            hash = 2
+            blockheader = 2
+
         return hash, blockheader
     calc_hash = np.vectorize(calc_hash)
 
     while True:
         if ctx.prevhash != ctx.updatedPrevHash:
             print('[*] New block {} detected on network '.format(ctx.prevhash))
-            print('[*] Best difficulty will trying to solve block {} was {}'.format(work_on+1, ctx.nHeightDiff[work_on+1]))
+            #print('[*] Best difficulty will trying to solve block {} was {}'.format(work_on+1, ctx.nHeightDiff[work_on+1]))
             ctx.updatedPrevHash = ctx.prevhash
             break
         hashes, blockheaders = calc_hash(nonces)
@@ -171,15 +175,15 @@ def bitcoin_miner(restarted=True):
         print(hashes[len(hashes) - 1])
         # hash meter, only works with regular nonce.
 
-        #last_updated = calculate_hashrate(nNonce, last_updated)
-
-        found_nonce = np.sum(np.isin(hashes<target, [True])) > 0
+        last_updated = calculate_hashrate(last_nonce, last_updated)
+        target = int(target, 16)
+        found_nonce = np.any(hashes<target)
         print(found_nonce)
         if found_nonce: # You Win!
-            nonce_index = np.where(hashes < target)[0]
-            nonce = nonces[nonce_index]
-
-            print('[yay!] Block {} solved.'.format(work_on+1))
+            nonce_index = np.where(np.any(hashes < target))[0]
+            nonce = format(nonces[nonce_index][0] + 1, "08x")
+            print(nonce)
+            print('[yay!] Block solved.')
             print('[*] Block hash: {}'.format(hashes[nonce_index]))
             print('[*] Blockheader: {}'.format(blockheaders[nonce_index]))            
             payload = bytes('{"params": ["'+address+'", "'+ctx.job_id+'", "'+ctx.extranonce2 \
@@ -209,30 +213,33 @@ def block_listener():
     if not working_now:
         # init a connection to ckpool 
         sock  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('btc.viabtc.io', 3333))
+        sock.connect(('asia-rvn.2miners.com', 6060))
         # send a handle subscribe message 
         sock.sendall(b'{"id": 1, "method": "mining.subscribe", "params": [""]}\n')
         lines = sock.recv(1024).decode().split('\n')
         response = json.loads(lines[0])
-        ctx.sub_details,ctx.extranonce1,ctx.extranonce2_size = response['result']
+        ctx.extranonce1,ctx.extranonce2_size = response['result']
         # send and handle authorize message  
-        sock.sendall(b'{"params": ["'+address.encode()+b'", "123"], "id": 2, "method": "mining.authorize"}\n')
+        sock.sendall(b'{"params": ["'+address.encode()+b'", "x"], "id": 2, "method": "mining.authorize"}\n')
         response = b''
         
-        while(True):
-            recv_payload = sock.recv(5120)
+        while True:
+            recv_payload = sock.recv(1024)
             response += recv_payload
-            if(response.decode().endswith("true]}\n")) or (response.decode().endswith("false]}\n")):
+            if ("mining.notify" in response.decode()) and (response.decode().endswith("\"]}\n")):
                 break
         responses = [json.loads(res) for res in response.decode().split('\n') if len(res.strip())>0 and 'mining.notify' in res]
-        ctx.job_id, ctx.prevhash, ctx.coinb1, ctx.coinb2, ctx.merkle_branch, ctx.version, ctx.nbits, ctx.ntime, ctx.clean_jobs = responses[0]['params']
-        print("Start mining!")
+
+        print(responses)
+        ctx.job_id, ctx.prevhash, ctx.coinb1, ctx.coinb2, ctx.clean_jobs, ctx.nbits, ctx.ntime = responses[0]['params']
+        ctx.version = "30000000"
         print("======= Job Information =======")
+        print(f"extranonce : {ctx.extranonce1}")
+        print(f"extranonce2_size : {ctx.extranonce2_size}")
         print(f"job_id : {ctx.job_id}")
         print(f"prev_hash : {ctx.prevhash}")
         print(f"coinbase1 : {ctx.coinb1}")
         print(f"coinbase2 : {ctx.coinb2}")
-        print(f"merkle_branch : {ctx.merkle_branch}")
         print(f"version : {ctx.version}")
         print(f"nbits : {ctx.nbits}")
         print(f"ntime : {ctx.ntime}")
@@ -243,19 +250,35 @@ def block_listener():
         # set sock 
         ctx.sock = sock 
         working_now = True
-
+        print("Good luck! ;)")
 
         # check for new block 
-        response = b''
-        while response.count(b'\n') < 4 and not(b'mining.notify' in response):response += sock.recv(5120)
-        print(response)
-        responses = [json.loads(res) for res in response.decode().split('\n') if len(res.strip())>0 and 'mining.notify' in res]     
+        #response = b''
+        #recv_payload = b''
+        #while True:
+        #    recv_payload = sock.recv(1024)
+        #    response += recv_payload
+        #    print(recv_payload)
+        #    if (response.decode() in "mining.notify") and (response.decode().endswith("\"]}\n")):
+        #        break
+        #print(response)
+        #responses = [json.loads(res) for res in response.decode().split('\n') if len(res.strip())>0 and 'mining.notify' in res]     
 
-        if responses[0]['params'][1] != ctx.prevhash:
+        if (not working_now) and responses[0]['params'][1] != ctx.prevhash:
             # new block detected on network 
             # update context job data 
-            ctx.job_id, ctx.prevhash, ctx.coinb1, ctx.coinb2, ctx.merkle_branch, ctx.version, ctx.nbits, ctx.ntime, ctx.clean_jobs = responses[0]['params']
-            
+            ctx.job_id, ctx.prevhash, ctx.coinb1, ctx.coinb2, ctx.clean_jobs, ctx.nbits, ctx.ntime = responses[0]['params']
+            print("======= Job Information =======")
+            print(f"job_id : {ctx.job_id}")
+            print(f"prev_hash : {ctx.prevhash}")
+            print(f"coinbase1 : {ctx.coinb1}")
+            print(f"coinbase2 : {ctx.coinb2}")
+            print(f"version : {ctx.version}")
+            print(f"nbits : {ctx.nbits}")
+            print(f"ntime : {ctx.ntime}")
+            print(f"clean_jobs : {ctx.clean_jobs}")
+            print("===============================")
+        ctx.nbits = str(ctx.nbits)
 
 
 
@@ -273,10 +296,10 @@ class CoinMinerThread(ExitedThread):
         check_for_shutdown(self)
         try:
             ret = bitcoin_miner(self)
-            logg("[*] Miner returned %s\n\n" % "true" if ret else"false")
+            print("[*] Miner returned %s\n\n" % "true" if ret else"false")
         except Exception as e:
-            logg("[*] Miner()")
-            logg(e)
+            print("[*] Miner()")
+            print(e)
             traceback.print_exc()
         ctx.listfThreadRunning[self.n] = False
 
@@ -298,8 +321,8 @@ class NewSubscribeThread(ExitedThread):
         try:
             ret = block_listener()
         except Exception as e:
-            logg("[*] Subscribe thread()")
-            logg(e)
+            print("[*] Subscribe thread()")
+            print(e)
             traceback.print_exc()
         ctx.listfThreadRunning[self.n] = False
 
@@ -313,10 +336,7 @@ def StartMining():
     while True:
         print("Start mining...")
         block_listener()
-        logg("[*] Subscribe thread started.")
-
         bitcoin_miner()
-        logg("[*] Bitcoin miner thread started")
 
         print('Bitcoin Miner started')
 
